a8b74d36d92255a0f96b81879ba3c7d924de5d69
Pacal2 <i-jackie@hotmail.com>
2020-10-02 18:20:15 +0200

    functions, arrays and global variables

--- script.js
+++ script.js
@@ -51,3 +51,52 @@ var thisStrLength = thisStr.length;
 console.log(thisStrLength);
 var thisStrFirstLetter = thisStr[0];
 console.log(thisStrFirstLetter);
+var thisStrLastLetter = thisStr[thisStrLength-1];
+console.log(thisStrLastLetter);
+
+// Functions
+function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+	
+	var result = "";
+	result += "The " + myAdjective + " " + myNoun + " " + myVerb + " " + myAdverb;
+	return result;
+		
+}
+
+console.log(wordBlanks("dog", "big", "ran", "quickly"));
+console.log(wordBlanks("cat", "small", "sneaked", "slowly"));
+
+// Arrays
+var myArray = ["Pacal", 28];
+console.log(myArray);
+var myData = myArray[0];
+console.log(myData);
+myArray[1] = 82;
+console.log(myArray);
+
+// Multi-dimensional arrays
+var myMultiDimArray = [[1, 2, 3], [4, 5, 6], [7,8,9]]
+var getMultiDimData = myMultiDimArray[0][0];
+
+// Adding data to arrays
+myArray.push("Green");
+console.log(myArray);
+myArray.pop();
+console.log(myArray);
+myArray.shift();
+console.log(myArray);
+myArray.unshift("Pacal");
+console.log(myArray);
+
+// Global variables (no var in function or var outside function)
+var varGlobal = 10;
+function function2() {
+	globalVar = 5;
+}
+
+function functionUsingGlobal() {
+	var output = varGlobal + 20;
+	console.log(output);
+
+}
+functionUsingGlobal();
