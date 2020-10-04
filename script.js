9ae00c1b274faf1d734474e3b201f15e3d2192e9
Pacal2 <i-jackie@hotmail.com>
2020-10-04 17:25:59 +0200

    if statements, switch statements, objects

--- script.js
+++ script.js
@@ -97,6 +97,132 @@ function function2() {
 function functionUsingGlobal() {
 	var output = varGlobal + 20;
 	console.log(output);
-
 }
 functionUsingGlobal();
+
+var outsideVar = "Outside Variable";
+function outsideVariableInsideFunction() {
+	var outsideVar = "Inside Variable";
+	return outsideVar;
+}
+console.log(outsideVariableInsideFunction());
+console.log(outsideVar);
+
+// Queues
+function nextInLine(arr, item) {
+	arr.push(item);
+	return arr.shift();
+}
+var testArr = [1, 2, 3, 4, 5]
+
+console.log("Before: " + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6));
+console.log("After: " + JSON.stringify(testArr));
+
+// Boolean values and what IF statement
+function trueOrFalse (isItTrue) {
+	if (isItTrue) {
+		return "Yes, it's true";
+	}
+	
+	return "No, it's false";
+	
+}
+console.log(trueOrFalse(true));
+console.log(trueOrFalse(false));
+
+function isItEqualToTen(val) {
+	if (val == 10) {
+		return "It's equal to ten.";
+	} else {
+		return "It's not equal to ten."
+	}
+}
+
+console.log(isItEqualToTen(10));
+console.log(isItEqualToTen(12));
+console.log(12 == 10);
+console.log(10 == 10);
+console.log(10 > 9);
+console.log(10 < 9);
+
+/*
+3 === 3 true
+3 === "3" false
+*/
+
+function testLogicalAnd(val) {
+	if (val <= 50 && val >=25) {
+		return "Yes";
+	} else {
+		return "No";
+	}
+}
+console.log(testLogicalAnd(40));
+
+function teestElseIf(val) {
+	if (val > 10) {
+		return "Greater than 10"; 
+	} else if (val < 5) {
+		return "Smaller than 5";
+	} else {
+		return "Between 5 and 10";
+	}	
+}
+
+// Switch statements
+function caseInSwitch(val) {
+	var answer = "";
+	
+	switch(val) {
+		case 1:
+			answer = "alpha";
+			break;
+		case 2:
+			answer = "beta";
+			break;
+		case 3:
+			answer = "gamma";
+			break;
+		case 4:
+			answer = "delta";
+			break;
+		default:
+			answer = "omega";
+			break;
+	}
+	
+	return answer;
+}
+console.log(caseInSwitch(1));
+console.log(caseInSwitch(3));
+console.log(caseInSwitch(5));
+
+// Funtion returning booleans
+function isLess(a, b) {
+	return a < b;
+}
+console.log(isLess(10, 15));
+
+// Function for diagonal
+function ABDiagonal(a, b) {
+	
+	if(a < 0 || b < 0) {
+		return undefined;
+	}
+	
+	return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+}
+
+// Objects
+var myDog = {
+	"name": "Reksio",
+	"legs": 4,
+	"tails": 1,
+	"best friends": ["Bumba", "Pampers"]
+};
+
+var dogName = myDog.name;
+console.log(dogName);
+var dogFriends = myDog["best friends"];
+console.log(JSON.stringify(dogFriends));
