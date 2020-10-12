57fbded1a7a17fb2b26eeb4e4d1888dfda3c9772
Pacal2 <i-jackie@hotmail.com>
2020-10-12 19:08:16 +0200

    spread and rest operators

--- script.js
+++ script.js
@@ -502,15 +502,50 @@ const arrowFunction = () => new Date();
 const myConcat = (arr1, arr2) => arr1.concat(arr2);
 console.log(myConcat([1, 2], [3, 4, 5]));
 
+// Higher order arrow functions
+// part.1
+const realNumberArray = [4, 5.6, -9.8, 3.14, 42, 6, 8.34, -2];
+
+const squareList = (arr) => {
+	const squaredIntegers = arr.filter(num => Number.isInteger(num) && num > 0).map(x => x * x);
+	
+	return squaredIntegers
+	
+}
+
+const squaredIntegers = squareList(realNumberArray);
+console.log("Squared integers " + squaredIntegers);
 
 
+// part.2
+const increment = (function() {
+	return function increment (number, value = 1) {
+		return number + value;
+	};
+})();
+console.log(increment(5, 2));
+console.log(increment(5));
 
+// Rest operator(...)
+const sumFunction = (function() {
+	return function(...args) {
+		//const args = [x, y, x];
+		return args.reduce((a, b) => a + b, 0);
+	};
+})();
+console.log(sumFunction(1, 2, 3));
+console.log(sumFunction(1, 2, 3, 4));
+console.log(sumFunction(1, 2, 3, 4, 5));
 
-
-
-
-
-
+// Spread operator
+const spreadArr1 = ["apple", "tomato", "banana", "carrot"];
+let spreadArr2;
+(function() {
+	spreadArr2 = [...spreadArr1];
+	spreadArr1[0] = "potato";
+})();
+console.log(spreadArr1);
+console.log(spreadArr2);
 
 
 
