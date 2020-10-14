ec11ac3d9bd68fd4aba74d522ac74a83f41653ab
Pacal2 <i-jackie@hotmail.com>
2020-10-14 18:33:43 +0200

    Destructuring assignements

--- script.js
+++ script.js
@@ -547,18 +547,89 @@ let spreadArr2;
 console.log(spreadArr1);
 console.log(spreadArr2);
 
+// Destructuring asignements.
+var voxel = {x: 3.6, y: 7.4, z: 6.54};
 
+// Traditional way to asign values:
+var xVoxel = voxel.x;
+var yVoxel = voxel.y;
+var zVoxel = voxel.z;
 
+const { xVoxel : aVoxel, yVoxel : bVoxel, zVoxel : cVoxel } = voxel;
 
+const AVG_TEMPERATURES = {
+	today: 27.5,
+	tomorrow: 27
+}
 
+function getTempOfTomorrow(avgTemperatures) {
+	"use strict";
+	
+	const { tomorrow: tempOfTomorrow } = avgTemperatures;
+	
+	return tempOfTomorrow;
+}
 
+console.log(getTempOfTomorrow(AVG_TEMPERATURES));
 
+// Destructuring Assignement with Nested Objects
+const LOCAL_FORECAST = {
+	today:  {min: 22, max: 28.7},
+	tomorrow: { min: 23.2, max : 27 }
+};
 
+function getMaxOfTomorrow(forecast) {
+	"use strict";
+	
+	const { tomorrow : { max : maxOfTomorrow} } = forecast;
+	
+	return maxOfTomorrow;
+}
 
+console.log(getMaxOfTomorrow(LOCAL_FORECAST));
 
+//Destructuring to assign array variables
+const [zAssigned, xAssigned, , yAssigned] = [1, 2, 3, 4, 5, 6];
+console.log(zAssigned, xAssigned, yAssigned);
 
+let aAssigned = 8, bAssigned = 6;
+(() => {
+	"use strict";
+	[a, b] = [b, a];
+})();
+console.log(aAssigned);
+console.log(bAssigned);
 
+// Destructuring with the rest operator.
+const sourceArray = [1,2,3,4,5,6,7,8,9,10];
+function removeFirstTwo(list) {
+	const [ , , ...arr] = list;
+	
+	return arr;
+}
 
+const arrayForDestRestTest = removeFirstTwo(sourceArray);
+console.log(arrayForDestRestTest);
+console.log(sourceArray);
 
+// Destructuring assignements to pass objects as function's parameters
+const stats = {
+	max: 56.78,
+	standard_deviation: 4.34,
+	median: 34,
+	mode: 23.87,
+	min: -0.75,
+	average: 35.85
+};
 
+const half = (function() {
+	
+	return function half({max, min}) {
+		return(max + min) / 2.0;
+	};
+})();
 
+console.log(stats);
+console.log(half(stats));
+
+// Template literals
